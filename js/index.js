26c9de36006f31833e045ecfe00bfcf71e1d78dc
Jon <jon2328@hotmail.com>
2023-06-10 15:34:27 +0800

    index page, wip register page

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function onPhoneNumberInput(evt) {
+  const maxDigit = 10
+  // Only digit
+  if (/\D/g.test(evt.target.value)) {
+    evt.target.value = evt.target.value.replace(/\D/g, '')
+  }
+
+  if (evt.target.value.length > maxDigit) {
+    evt.target.value = evt.target.value.substring(0, evt.target.value.length - 1)
+  }
+
+}
+
+function checkLoyaltyPoint() {
+
+  const countryCode = document.querySelector('#country-code')
+  const phoneNum = document.querySelector('#phone-num')
+  const resultPhone = countryCode.value + phoneNum.value
+  if (resultPhone === '+60173527250') {
+    document.querySelector('#error').classList.remove('show')
+    window.location.href = "./register.html"
+  } else {
+    document.querySelector('#error').classList.add('show')
+  }
+}
